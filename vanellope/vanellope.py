e50bb4759a74309576ca83b581092e5b680737ed
dunossauro <mendesxeduardo@gmail.com>
2019-03-16 16:15:45 -0300

    Change behavior of rotate_block to exchange with previous block

--- vanellope/vanellope.py
+++ vanellope/vanellope.py
@@ -1,9 +1,11 @@
+"""Functions to glitch files."""
 from random import choice
 
 
 def rotate_blocks(blocks: list, n: int):
+    """Choose blocks randomly and exchange them with the previous blocks."""
     for x in range(n):
-        int_a = choice(range(1, len(blocks)))
+        int_a = choice(range(600, len(blocks)))
         int_b = int_a - 1
         blocks[int_a], blocks[int_b] = blocks[int_b], blocks[int_a]
 
